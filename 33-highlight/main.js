bc74ba62dbf8954d0495778ff18a594ef03707b0
hcq0313 <853801007@qq.com>
2019-04-03 16:00:30 +0800

    highlight

---
+++ 33-highlight/main.js
@@ -0,0 +1,13 @@
+/*global echarts: true */
+$(function(){
+  var $btnAddCode = $('input[type="button"]');
+
+  $btnAddCode.click(function(){
+    var $code = $('<div><pre class="javascript"></pre></div>');
+    $code.find('pre').html($('.code').val());
+    $('div.main').append($code);
+  
+  hljs.highlightBlock($code.find('pre').get(0));
+
+  });
+});
